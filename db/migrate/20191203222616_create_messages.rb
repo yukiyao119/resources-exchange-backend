f685dac2513efcbf60cf1e1a88b91ba4bf007d34
yukiyao119 <ukiyao@hotmail.com>
2019-12-03 17:40:26 -0500

    message model created

--- db/migrate/20191203222616_create_messages.rb
+++ db/migrate/20191203222616_create_messages.rb
@@ -1,10 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.user_id :integer
-      t.exchange_id :integer
-      t.content :string
-
+      t.integer :user_id
+      t.integer :exchange_id
+      t.string :content
+      
       t.timestamps
     end
   end
