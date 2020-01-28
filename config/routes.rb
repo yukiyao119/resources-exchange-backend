09b83661364f675e8019bdd11d0ce3c8c5a41c57
yukiyao119 <ukiyao@hotmail.com>
2020-01-27 21:57:37 -0500

    reverse the change

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,6 @@ Rails.application.routes.draw do
   match '*_missing_page', to: 'users#profile', via: :get
 
 
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-  !request.xhr? && request.format.html?
-  end
-
-
   mount ActionCable.server => '/cable'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
