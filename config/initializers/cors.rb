fac21d3c74e7d478f37d7ab38d4be36e50fed5d7
yukiyao119 <ukiyao@hotmail.com>
2020-02-16 02:48:55 -0500

    change cors to *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://naughty-bhabha-f00715.netlify.com/'
+    origins '*'
+    # 'https://naughty-bhabha-f00715.netlify.com/'
     # , 'YOUR-CUSTOM-DOMAIN'
 
     resource '*',
